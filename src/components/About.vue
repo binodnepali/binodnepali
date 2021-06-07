ed1c28a8f371c8cde5225728b455f221dc0955fd
Binod <nepalibinod9@gmail.com>
2021-06-07 17:08:32 +0200

    Setup
    Initial setup using vite

---
+++ src/components/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Hello, I am Binod Nepali. This is my portfolio website. Currently, I am
+    working on it
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'About',
+})
+</script>
